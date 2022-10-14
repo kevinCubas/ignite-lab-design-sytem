916fc87979a7042593f8e05fbfd073c58f020cea
unknown <61201131+kevinCubas@users.noreply.github.com>
2022-10-14 01:46:39 -0300

    create component and structure the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+export function App() {
+
+  return (
+    <div className="App">
+      <h1 className="font-bold text-5xl text-violet-500">Hello</h1>
+    </div>
+  )
+}
+
